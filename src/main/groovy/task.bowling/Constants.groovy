dd5918cf6fee898b097a44b216037a54e6b360f2
AntonCheliadin <chel_anton@mail.ru>
2017-01-26 10:15:21 +0200

    move constants to separate class

---
+++ src/main/groovy/task.bowling/Constants.groovy
@@ -0,0 +1,11 @@
+package task.bowling
+
+class Constants {
+    public static final Integer MIN_PIN = 0
+    public static final Integer MAX_PIN = 10
+    public static final Integer FIRST_FRAME_NUMBER = 1
+    public static final Integer LAST_FRAME_NUMBER = 10
+
+    public static final String BONUS_PINS = "bonus_pins"
+    public static final String HIT_PINS = "pins"
+}
